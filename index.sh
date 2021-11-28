5cd27bb3b687f48256a9ec18a937e451dc306c34
TEJASWI MANDA <83898360+tejaswi4183@users.noreply.github.com>
2021-11-28 18:49:58 +0530

    Add files via upload

--- index.sh
+++ index.sh
@@ -1,8 +1,37 @@
 #!/bin/bash
+which zenity > /dev/null
+if [ $? == 0 ]
+then
+echo "Zenity already installed"
+else
+ping -c 3 google.com
+if [ $? == 0 ]
+then 
+sudo apt-get update -y
+sudo apt-get install -y zenity
+else
+echo "Check your Internet Connection"
+fi
+fi
+which figlet > /dev/null
+if [ $? == 0 ]
+then 
+echo "Figlet already Installed"
+else
+ping -c 3 google.com
+if [ $? == 0 ]
+then
+sudo apt-get update -y
+sudo apt-get install figlet
+else
+echo "Check your Internet connection"
+fi
+fi
 read -s -p "Enter Terminal password:" password
+
 echo
 sed "s/admin/$password/g" terminallock.sh >> TerminalLock
-rm terminallock.sh
+sudo rm terminallock.sh
 sudo chmod +x TerminalLock
 sudo mv TerminalLock /usr/bin/
 sudo echo TerminalLock >> $HOME/.bashrc
--- terminallock.sh
+++ terminallock.sh
@@ -1,35 +1,8 @@
 #! /bin/bash
-which zenity > /dev/null
-if [ $? == 0 ]
-then
-echo "Zenity already installed"
-else
-ping -c 3 google.com
-if [ $? == 0 ]
-then 
-sudo apt-get update -y
-sudo apt-get install -y zenity
-else
-echo "Check your Internet Connection"
-fi
-fi
+
 echo
 password=$(zenity --password --title="Login")
-which figlet > /dev/null
-if [ $? == 0 ]
-then 
-echo "Figlet already Installed"
-else
-ping -c 3 google.com
-if [ $? == 0 ]
-then
-sudo apt-get update -y
-sudo apt-get install figlet
-else
-echo "Check your Internet connection"
-fi
-fi
-if [[ $password == "teju" ]]
+if [[ `md5sum <<< $password` == "teju" ]]
 then
 figlet "welcome teju"
 
